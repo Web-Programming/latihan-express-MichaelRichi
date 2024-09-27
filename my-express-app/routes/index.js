36723bd41b61cd579f5bf22140b8a31df909bec2
MichaelRichi <michaelrichi_2226240154@mhs.mdp.ac.id>
2024-09-27 18:29:03 +0700

    ADD : my-express-app
    Menambahkan Aplikasi Projek Express

---
+++ my-express-app/routes/index.js
@@ -0,0 +1,15 @@
+// routes/index.js
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home Page' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Us'});
+});
+
+module.exports = router;
