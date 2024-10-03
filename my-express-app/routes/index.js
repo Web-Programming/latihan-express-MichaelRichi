89d961a68ce2c6e774b844d9a32eba6a712daa95
MichaelRichi <michaelrichi_2226240154@mhs.mdp.ac.id>
2024-10-03 23:31:29 +0700

    ADD : Contact and Submit Contact
    Menambahkan halaman kontak dan kumpul kontak

--- my-express-app/routes/index.js
+++ my-express-app/routes/index.js
@@ -3,13 +3,27 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home Page' });
 });
 
+/* GET about page. */
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About Us'});
+});
+
+/* GET contact us page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Us'});
+});
+
+/* Post Submit Contact page. */
+router.post('/submit-contact', (req, res) => {
+  const { name, email, message } = req.body;
+  console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+  res.render('submit-contact', {title: 'Thank You', name : name}); 
 });
 
 module.exports = router;
