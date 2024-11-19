07cd4567cd90b218ba52ca138e86147336b90f71
MichaelRichi <michaelrichi_2226240154@mhs.mdp.ac.id>
2024-11-19 23:59:43 +0700

    Update : Housing Service

--- my-belajar-Angular/src/app/home/home.component.ts
+++ my-belajar-Angular/src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housing-location';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
@@ -11,26 +12,10 @@ import { HousingLocation } from '../housing-location';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  housingLocationList: HousingLocation[] = [
-    {
-      id: 0,
-      name: "Citra Grand City",
-      city: "Palembang",
-      state: "Sumsel",
-      photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTta7Pk3A7e0JkMLOgt16mr34t9_QMa2J99UQ&s",
-      availableUnits: 12,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 1,
-      name: "Citra Land",
-      city: "Palembang",
-      state: "Sumsel",
-      photo: "https://cdn.lugc.link/d9447493-deeb-4408-bfd5-a621353fe621/-/preview/278x183/-/format/auto/",
-      availableUnits: 12,
-      wifi: true,
-      laundry: true
-    },
-  ]
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+  this.housingLocationList = this.housingService.getAllHousingLocations(); 
+  }
 }
---
+++ my-belajar-Angular/src/app/housing.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HousingLocation } from './housing-location';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HousingService {
+  protected housingLocationList: HousingLocation[] = [
+    {
+      id: 0,
+      name: "Citra Grand City",
+      city: "Palembang",
+      state: "Sumsel",
+      photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTta7Pk3A7e0JkMLOgt16mr34t9_QMa2J99UQ&s",
+      availableUnits: 12,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 1,
+      name: "Citra Land",
+      city: "Palembang",
+      state: "Sumsel",
+      photo: "https://cdn.lugc.link/d9447493-deeb-4408-bfd5-a621353fe621/-/preview/278x183/-/format/auto/",
+      availableUnits: 20,
+      wifi: true,
+      laundry: true
+    },
+  ];
+
+  constructor() { }
+
+  getAllHousingLocations() : HousingLocation[] {
+    return this.housingLocationList;
+  }
+
+  getHousingLocationById(id: number): HousingLocation | undefined {
+    return this.housingLocationList.find(housingLocation => housingLocation.id == id);
+  }
+}
